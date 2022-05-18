51a112c7032b6bd032d12443167056c5d7a53d1d
juniokoi <juniokoi@hotmail.com>
2022-05-18 17:05:59 -0300

    Bateria finalizada, resta tocar pelo mouse

---
+++ index.js
@@ -0,0 +1,37 @@
+const setLayout = document.querySelector( '.set' ).children.length;
+
+const button = document.querySelector( '.a' );
+
+document.addEventListener( 'keydown', ( event ) => {
+  let key = event.key;
+  CheckKey( key );
+} );
+
+
+const keyLayout = [
+  [ "w", "tom-1" ],
+  [ "a", "tom-2" ],
+  [ "s", "tom-3" ],
+  [ "d", "tom-4" ],
+  [ 'j', "crash" ],
+  [ "k", "kick-bass" ],
+  [ "l", "snare" ],
+];
+
+
+
+function CheckKey ( keyName ) {
+  for ( let i = 0; i < setLayout; i++ ) {
+    if ( keyLayout[ i ][ 0 ] == keyName ) {
+      PlaySound( keyLayout[ i ][ 1 ] );
+    }
+  }
+};
+
+function PlaySound ( soundName ) {
+  const audio = new Audio( `/sounds/${soundName}.mp3` );
+  audio.muted = false;
+  audio.autoplay = false;
+  audio.play();
+  audio.volume = 0.5;
+}
